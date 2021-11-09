7403911bc5321cb114f432c39bd04ca76d97bc21
qb <saschap@mail.de>
2021-11-09 16:31:32 +0100

    .

--- src/triggerbot.nim
+++ src/triggerbot.nim
@@ -1,5 +1,5 @@
 import globals, input
 
-proc checkTrigger*(self: Entity, e: Entity) =
+proc checkTrigger*(self: Entity, e: var Entity) =
   if self.team != e.team and self.crossId == e.id:
     clickMouse()
