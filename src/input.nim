fcd1c2f35b1d31e70cbdfa2cf089e60342115740
qb <saschap@mail.de>
2021-11-11 17:51:02 +0100

    .

--- src/input.nim
+++ src/input.nim
@@ -1,14 +1,13 @@
 import x11/[xlib, xtst]
 include x11/keysym
 
-var 
-  display = XOpenDisplay(nil)
-  keyMap: array[0..31, char]
+var display = XOpenDisplay(nil)
 
 proc keyPressed*(key: culong): bool =
-  discard XQueryKeymap(display, keyMap)
-  let triggerKeyCode = XKeysymToKeycode(display, key)
-  ord(keyMap[triggerKeyCode.int shr 3]) != 0 and (1 shl (triggerKeyCode.int and 7)) > 0
+  var keys: array[0..31, char]
+  discard XQueryKeymap(display, keys)
+  let keycode = XKeysymToKeycode(display, key)
+  (ord(keys[keycode.int div 8]) and (1 shl (keycode.int mod 8))) != 0
 
 proc clickMouse* =
   discard XTestFakeButtonEvent(display, 1, 1, 0)
